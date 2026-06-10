e74528c76fc1efaaf98157ab9a770d178e56d043
raartist <rishabhzee98@gmail.com>
2026-06-10 23:10:20 +0530

    chore: register route implementation with password column added to db

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../auth/auth');
+const bcrypt = require('bcrypt');
 
 const db = require('../db');
 
@@ -90,6 +91,25 @@ router.patch("/:id",async(req,res)=>{
         res.status(500).json({error:"Internal Server Error"});
     }
 });
+
+router.post("/register",async(req,res)=>{
+
+   const {name,email,password} = req.body;
+   const hashedPassword = await bcrypt.hash(password,10);
+
+   const result = await db.query(
+      `
+      INSERT INTO users(name,email,password)
+      VALUES($1,$2,$3)
+      RETURNING id,name,email
+      `,
+      [name,email,hashedPassword]
+   );
+
+   res.status(201).json(result.rows[0]);
+});
+
+
 
 router.get(
    "/profile",

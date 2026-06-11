74c2402533f595ae7bb56f628abafa8658ffbbca
raartist <rishabhzee98@gmail.com>
2026-06-11 13:00:14 +0530

    chore: securing route with jwt tokens

--- auth/auth.js
+++ auth/auth.js
@@ -1,13 +1,27 @@
-function authMiddleware(req, res, next) {
-  const token = req.headers.authorization
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "secretkey123";
 
-  if (token !== 'secret123') {
+function authMiddleware(req, res, next) {
+  const token = req.headers.authorization;
+  //   const token = bearer_token && bearer_token.split(" ")[1];
+
+  if (!token) {
     return res.status(401).json({
-      message: 'unauthorized',
-    })
+      message: "Token required!",
+    });
   }
-  console.log('authorized user')
-  next()
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    console.log(decoded, "decoded token");
+    req.user = decoded;
+  } catch (err) {
+    console.error(err);
+    return res.status(401).json({
+      message: "Invalid token!",
+    });
+  }
+  console.log("authorized user");
+  next();
 }
 
-module.exports = authMiddleware
+module.exports = authMiddleware;
